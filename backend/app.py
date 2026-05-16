2d04da3bbd1a2cf152dd0e99ebd518b1743fecdf
Jhon Saillon <jhonsaillon54@gmail.com>
2026-05-16 01:39:00 -0300

    fix cors render

--- backend/app.py
+++ backend/app.py
@@ -2,4 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+
+CORS(
+    app,
+    resources={r"/*": {"origins": "*"}},
+    supports_credentials=True
+)
