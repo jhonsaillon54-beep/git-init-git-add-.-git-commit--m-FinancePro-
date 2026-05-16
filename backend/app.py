63b5d33bd4af6da44a7e4fc897bdf1416ee907ec
Jhon Saillon <jhonsaillon54@gmail.com>
2026-05-16 01:28:57 -0300

    fix cors

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,4 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-from flask_cors import CORS
-
-CORS(app)
-
-from routes.auth import auth_bp
-
-app.register_blueprint(auth_bp, url_prefix="/api/auth")
+CORS(app, resources={r"/*": {"origins": "*"}})
