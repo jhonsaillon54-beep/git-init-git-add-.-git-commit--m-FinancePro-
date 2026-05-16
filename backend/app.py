a9f4e22baa6ac6397ba21c239420d930ff56fbf7
Jhon Saillon <jhonsaillon54@gmail.com>
2026-05-16 00:56:31 -0300

    fix cors vercel

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from routes.auth import auth_bp
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Rotas
 app.register_blueprint(auth_bp, url_prefix="/api/auth")
